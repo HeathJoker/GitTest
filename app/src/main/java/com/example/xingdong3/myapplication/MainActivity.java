53298fc1f3d6c805ac5b6767727469c504f04219
xingdong3 <xingdong3@staff.weibo.com>
2017-08-25 12:03:32 +0800

    第一次提交啊

---
+++ app/src/main/java/com/example/xingdong3/myapplication/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.xingdong3.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    int i;
+    int j;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+}
