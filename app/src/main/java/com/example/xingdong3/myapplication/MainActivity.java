7fa0432132a11063c45d0954c6f5061b62716115
xingdong3 <xingdong3@staff.weibo.com>
2017-08-25 14:47:58 +0800

    增加get set

--- app/src/main/java/com/example/xingdong3/myapplication/MainActivity.java
+++ app/src/main/java/com/example/xingdong3/myapplication/MainActivity.java
@@ -6,6 +6,23 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     int i;
+
+    public int getI() {
+        return i;
+    }
+
+    public void setI(int i) {
+        this.i = i;
+    }
+
+    public int getJ() {
+        return j;
+    }
+
+    public void setJ(int j) {
+        this.j = j;
+    }
+
     int j;
 
     @Override
